52dd47d9aa13846f807734f23f289e132e4474e2
김동영 <kdy0415@eltov.co.kr>
2025-07-03 12:57:00 +0900

    ㅇㅇ

--- src/app/(webpage)/references/page.js
+++ src/app/(webpage)/references/page.js
@@ -1,27 +1,18 @@
 'use client';
 
-import SubVisual from '../components/partials/subVisual/SubVisual';
+import SubVisual from '@/components/partials/subVisual/SubVisual';
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import styles from "./page.module.scss";
 
 export default function References() {
-    const [image, setImage] = useState("https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=800&q=80");
-    const title = {"ko": "포트폴리오", "en": "REFERENCS" , };
-    const subtext = "subtext";
+    const image = "https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=800&q=80";
+    const title = { "ko": "포트폴리오", "en": "REFERENCS" };
+    const subtext = { "ko": "포트폴리오11111", "en": "REFERENCS111111" };
 
     return (
         <>
             <SubVisual image={image} title={title} subtext={subtext} />
-            <div
-                className="sub_page_title"
-                style={{
-                    backgroundImage: `url('${image}')`,
-                }}
-            >
-                <h2>{title}</h2>
-                <div className="sub">{subtext}</div>
-            </div>
         </>
     );
 }
--- src/components/partials/subVisual/SubVisual.js
+++ src/components/partials/subVisual/SubVisual.js
@@ -4,13 +4,13 @@ import styles from "./Subvisual.module.scss";
 
 export default function SubVisual() {
     return (
-        <div className={style.sub_page_title}
+        <div className={styles.sub_page_title}
             style={{
                 backgroundImage: "url('https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=800&q=80')",
             }}
         >
             <h2>포트폴리오</h2>
-            <div className={style.sub}></div>
+            <div className={styles.sub}></div>
         </div>
     );
 }
